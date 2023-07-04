48b683627535e31f591b9203fba30b4651a77964
Andrés Meóñez <76024612+JoseanMeonez@users.noreply.github.com>
2023-07-04 16:35:54 -0600

    Adding files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers;
+
+public class HomeController : Controller
+{
+	private readonly ILogger<HomeController> _logger;
+
+	public HomeController(ILogger<HomeController> logger)
+	{
+		_logger = logger;
+	}
+
+	public IActionResult Index()
+	{
+		// if (HttpContext.Session.GetString("value") == null)
+		// {
+		// 	return RedirectToAction("Login", "Home");
+		// }
+
+		ViewData["Title"] = "Menú";
+		ViewData["Desc"] = "Menú Principal";
+		ViewData["Icon"] = "fa-solid fa-house-chimney";
+		ViewData["Breadcrumb"] = "Inicio";
+		return View();
+	}
+
+	public IActionResult Login()
+	{
+		return View();
+	}
+
+	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	public IActionResult Error()
+	{
+		return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+	}
+}
